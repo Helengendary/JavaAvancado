118aa3debfbb7d12eb5ae9d289a8db0a1e142e59
Helengendary <helenapicinin13@gmail.com>
2024-10-29 11:37:58 -0300

    Backenda

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.dto.LoginData;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @PostMapping
+    public ResponseEntity<String> login(@RequestBody LoginData data) {
+        
+        if (data.Login().equals("don") && data.Password().equals("1234")) 
+            return ResponseEntity.ok("Bem-vindo!");
+        
+        return ResponseEntity.status(404).build();
+    
+    }
+    
+}
